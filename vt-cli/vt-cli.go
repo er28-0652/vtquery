c99b4a7d439f3e1a36b4f276100a1e7238a58675
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-01-20 04:28:05 +0900

    add vt-cli.go

---
+++ vt-cli/vt-cli.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/urfave/cli"
+)
+
+func checkQueryType(query string) string {
+	// regexp for MD5, SHA1 and SHA256
+	hashPtn := regexp.MustCompile(`(^[a-fA-F0-9]{32}$|^[a-fA-F0-9]{40}$|^[a-fA-F0-9]{64}$)`)
+
+	// regexp for URL pattern
+	urlPtn := regexp.MustCompile(`^https?://.*`)
+
+	if hashPtn.MatchString(query) == true {
+		return "hash"
+	} else if urlPtn.MatchString(query) == true {
+		return "url"
+	} else {
+		return ""
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "query, q",
+			Usage: "hash or url to query",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		vt := DefaultClient()
+		q := c.String("query")
+		t := checkQueryType(q)
+
+		var result interface{}
+		var err error
+		if t == "hash" {
+			result, err = vt.HashQuery(q)
+			if err != nil {
+				return err
+			}
+		} else if t == "url" {
+			result, err = vt.URLQuery(q)
+			if err != nil {
+				return err
+			}
+		}
+		fmt.Printf("%+v\n", result)
+		return nil
+	}
+	app.Run(os.Args)
+	return
+}
