45cdc7e7b55831d18c763c75086c1c76b99ac6da
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-01-21 02:06:09 +0900

    add tests for query

---
+++ query_test.go
@@ -0,0 +1,43 @@
+package vtquery
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHashQuery(t *testing.T) {
+	tests := []string{
+		"909349d9beeaf08a155bdfc8aadf73d093e545b7",
+		"61b5ac4b9440f41e2a7771445cf2f5d23cbdc8d7",
+	}
+	vt := NewDefaultClient()
+	for _, test := range tests {
+		result, err := vt.HashQuery(test)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Attributes.Sha1 != test {
+			t.Errorf("got=%#v\nwant=%#v", result.Attributes.Sha1, test)
+		}
+	}
+}
+
+func TestHashQuery_fail(t *testing.T) {
+	vt := NewDefaultClient()
+	t.Run("invalid query case", func(t *testing.T) {
+		_, err := vt.HashQuery("hogehoge")
+		expect := "invalid hash query"
+		if err.Error() != expect {
+			t.Errorf("got=%#v\nwant=%#v", err, expect)
+		}
+	})
+
+	t.Run("not found case", func(t *testing.T) {
+		q := "61b5ac4b9440f41e2a7771445cf2f5d23cbdc8d9"
+		_, err := vt.HashQuery(q)
+		expect := fmt.Sprintf("\"%s\" is not found in VT", q)
+		if err.Error() != expect {
+			t.Errorf("got=%#v\nwant=%#v", err, expect)
+		}
+	})
+}
