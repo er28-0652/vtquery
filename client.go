bce213518dba0ea9bb10e85977a7eb1b6466e489
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-01-20 18:15:59 +0900

    update comment for NewDefaultClient

--- client.go
+++ client.go
@@ -46,7 +46,7 @@ func NewClient(baseURL, userAgent string, insecure bool) (*Client, error) {
 	return client, nil
 }
 
-// DefaultClient creates instance with default URL and User-Agent
+// NewDefaultClient creates instance with default URL and User-Agent
 func NewDefaultClient() *Client {
 	baseURL := "https://www.virustotal.com"
 	userAgent := "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
