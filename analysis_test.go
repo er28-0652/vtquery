47c08007b74f5047f3bb250df46a51fa658b59f9
samsepi01 <33626923+er28-0652@users.noreply.github.com>
2018-03-04 04:53:44 +0900

    update: add test TestShowReportURL

--- analysis_test.go
+++ analysis_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestShowReport(t *testing.T) {
+func TestShowReportHash(t *testing.T) {
 	tests := []string{
 		"909349d9beeaf08a155bdfc8aadf73d093e545b7",
 		"61b5ac4b9440f41e2a7771445cf2f5d23cbdc8d7",
@@ -15,6 +15,26 @@ func TestShowReport(t *testing.T) {
 	}
 	for _, test := range tests {
 		result, err := vt.HashQuery(test)
+		if err != nil {
+			t.Fatal(err)
+		}
+		result.ShowReport()
+	}
+}
+
+func TestShowReportURL(t *testing.T) {
+	tests := []string{
+		// TAKE CARE! this is malicious
+		"http://jpcerts.jpcertinfo.com/",
+		// this is normal
+		"https://www.google.com",
+	}
+	vt, err := NewDefaultClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, test := range tests {
+		result, err := vt.URLQuery(test)
 		if err != nil {
 			t.Fatal(err)
 		}
