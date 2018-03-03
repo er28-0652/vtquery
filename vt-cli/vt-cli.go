b67fcac054e10be2151e985c426b658f1ad99235
samsepi01 <33626923+er28-0652@users.noreply.github.com>
2018-03-04 06:22:07 +0900

    update: update to v1.0.1

--- vt-cli/vt-cli.go
+++ vt-cli/vt-cli.go
@@ -35,7 +35,7 @@ func query(c *cli.Context) error {
 
 func main() {
 	app := cli.NewApp()
-	app.Version = "1.0.0"
+	app.Version = "1.0.1"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "query, q",
