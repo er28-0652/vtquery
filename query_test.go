17b93793a5b23ea9ef86c585860ee472a2f0e07d
samsepi01 <33626923+er28-0652@users.noreply.github.com>
2018-03-04 04:05:28 +0900

    change: add error checking for NewDefaultClient

--- query_test.go
+++ query_test.go
@@ -10,7 +10,10 @@ func TestHashQuery(t *testing.T) {
 		"909349d9beeaf08a155bdfc8aadf73d093e545b7",
 		"61b5ac4b9440f41e2a7771445cf2f5d23cbdc8d7",
 	}
-	vt := NewDefaultClient()
+	vt, err := NewDefaultClient()
+	if err != nil {
+		t.Fatal(err)
+	}
 	for _, test := range tests {
 		result, err := vt.HashQuery(test)
 		if err != nil {
@@ -23,7 +26,10 @@ func TestHashQuery(t *testing.T) {
 }
 
 func TestHashQuery_fail(t *testing.T) {
-	vt := NewDefaultClient()
+	vt, err := NewDefaultClient()
+	if err != nil {
+		t.Fatal(err)
+	}
 	t.Run("invalid query case", func(t *testing.T) {
 		_, err := vt.HashQuery("hogehoge")
 		expect := "invalid hash query"
